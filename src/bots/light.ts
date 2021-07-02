9ef181de0cb6aeb3d71f3fef618e0e4d20de5bf3
Martin Skalický <skalima7@fit.cvut.cz>
2021-07-02 18:08:32 +0200

    Prepare SPA

--- src/bots/light.ts
+++ src/bots/light.ts
@@ -1,96 +1,96 @@
-import { Platform } from "../lib/connection";
-import { ComponentType, PropertyDataType } from "../lib/type";
+import { Platform } from '../lib/connection';
+import { ComponentType, PropertyDataType } from '../lib/type';
 
 async function main() {
-    const plat = new Platform("BOT-91JK123", "martas", "Světlomat");
-    const nodeLight = plat.addNode("light", "Světlo", ComponentType.switch);
-    nodeLight.addProperty({
-        propertyId: "power",
-        dataType: PropertyDataType.boolean,
-        name: "Světlo",
-        settable: true,
-    })
+  const plat = new Platform('BOT-91JK123', 'martas', 'Světlo');
+  const nodeLight = plat.addNode('light', 'Světlo', ComponentType.switch);
+  nodeLight.addProperty({
+    propertyId: 'power',
+    dataType: PropertyDataType.boolean,
+    name: 'Světlo',
+    settable: true,
+  });
 
-    nodeLight.addProperty({
-        propertyId: "current",
-        dataType: PropertyDataType.integer,
-        name: "Proud",
-        settable: true,
-        format: "0:16"
-    })
+  // nodeLight.addProperty({
+  //     propertyId: "current",
+  //     dataType: PropertyDataType.integer,
+  //     name: "Proud",
+  //     settable: true,
+  //     format: "0:16"
+  // })
 
-    nodeLight.addProperty({
-        propertyId: "kill",
-        dataType: PropertyDataType.boolean,
-        name: "Zabít",
-        settable: true
-    })
+  // nodeLight.addProperty({
+  //     propertyId: "kill",
+  //     dataType: PropertyDataType.boolean,
+  //     name: "Zabít",
+  //     settable: true
+  // })
 
-    const node = plat.addNode("center", "Centrum", ComponentType.generic);
-    node.addProperty({
-        name: "Displej",
-        propertyId: "currency",
-        dataType: PropertyDataType.enum,
-        format: "BTC,EUR,CZK,ETH",
-        settable: true,
-        callback: (prop) => console.log("currency is", prop.value)
-    });
+  // const node = plat.addNode("center", "Centrum", ComponentType.generic);
+  // node.addProperty({
+  //     name: "Displej",
+  //     propertyId: "currency",
+  //     dataType: PropertyDataType.enum,
+  //     format: "BTC,EUR,CZK,ETH",
+  //     settable: true,
+  //     callback: (prop) => console.log("currency is", prop.value)
+  // });
 
-    plat.addSensor({
-        name: "Aktuální teplota",
-        propertyId: "rate",
-        dataType: PropertyDataType.float,
-        unitOfMeasurement: "°C",
-    })
+  // plat.addSensor({
+  //     name: "Aktuální teplota",
+  //     propertyId: "rate",
+  //     dataType: PropertyDataType.float,
+  //     unitOfMeasurement: "°C",
+  // })
 
-    node.addProperty({
-        name: "Poměr",
-        propertyId: "percentage",
-        dataType: PropertyDataType.integer,
-        unitOfMeasurement: "%",
-        format: "0:100",
-        settable: true,
-        callback: (prop) => console.log("percentage is", prop.value)
-    });
+  // node.addProperty({
+  //     name: "Poměr",
+  //     propertyId: "percentage",
+  //     dataType: PropertyDataType.integer,
+  //     unitOfMeasurement: "%",
+  //     format: "0:100",
+  //     settable: true,
+  //     callback: (prop) => console.log("percentage is", prop.value)
+  // });
 
-    node.addProperty({
-        name: "Číslo",
-        propertyId: "numbers",
-        dataType: PropertyDataType.integer,
-        settable: true,
-        callback: (prop) => console.log("numbers is", prop.value)
-    });
+  // node.addProperty({
+  //     name: "Číslo",
+  //     propertyId: "numbers",
+  //     dataType: PropertyDataType.integer,
+  //     settable: true,
+  //     callback: (prop) => console.log("numbers is", prop.value)
+  // });
 
-    node.addProperty({
-        name: "Text",
-        propertyId: "strings",
-        dataType: PropertyDataType.string,
-        settable: true,
-        callback: (prop) => console.log("strings is", prop.value)
-    });
+  // node.addProperty({
+  //     name: "Text",
+  //     propertyId: "strings",
+  //     dataType: PropertyDataType.string,
+  //     settable: true,
+  //     callback: (prop) => console.log("strings is", prop.value)
+  // });
 
-    function sendRate() {
-        plat.publishSensorData(
-            "rate",
-            (Math.random() * 10).toFixed(2).toString()
-        );
-    }
+  // function sendRate() {
+  //     plat.publishSensorData(
+  //         "rate",
+  //         (Math.random() * 10).toFixed(2).toString()
+  //     );
+  // }
 
-    // plat.publishData("volt", "11");
-    plat.on("connect", (client) => {
-        console.log("sending data");
-        // plat.publishSensorData("volt", "11");
-        // plat.publishSensorData("temp", "21.1");
-        // plat.publishSensorData("hum", "74");
-        // plat.publishSensorData("power", "0");
+  // plat.publishData("volt", "11");
+  plat.on('connect', (client) => {
+    console.log('sending data');
+    // plat.publishSensorData("volt", "11");
+    // plat.publishSensorData("temp", "21.1");
+    // plat.publishSensorData("hum", "74");
+    // plat.publishSensorData("power", "0");
 
-        plat.publishData("light", "power", "true");
+    // plat.publishData('light', 'power', 'true');
 
-        sendRate();
-        setInterval(sendRate, 2000);
-        plat.publishData("center", "currency", "ETH");
-    });
-    plat.init();
+    // sendRate();
+    // setInterval(sendRate, 2000);
+    // plat.publishData('center', 'currency', 'ETH');
+  });
+  plat.init();
 }
 
 main();
--- src/bots/spa.ts
+++ src/bots/spa.ts
@@ -1,115 +1,107 @@
-import { Platform } from "../lib/connection";
-import { ComponentType, PropertyDataType, PropertyClass } from "../lib/type";
-import { Socket } from "net";
+import { Platform } from '../lib/connection';
+import { ComponentType, PropertyDataType, PropertyClass } from '../lib/type';
+import { Socket } from 'net';
 
 const config = {
-    spaIp: process.env.SPA_IP as string,
-    spaPort: Number(process.env.SPA_PORT),
-}
+  spaIp: process.env.SPA_IP as string,
+  spaPort: Number(process.env.SPA_PORT),
+};
 
-if (!config.spaIp)
-    console.error("INVALID spa IP")
+if (!config.spaIp) console.error('INVALID spa IP');
 
-if (!config.spaPort)
-    console.error("INVALID spa PORT")
+if (!config.spaPort) console.error('INVALID spa PORT');
 
 if (!config.spaPort || !config.spaIp) process.exit(1);
 
-const client = new Socket()
+const client = new Socket();
 
 const Bubbles = {
-    on: JSON.stringify({ "type": 1, "sid": "1616871584000", "data": "8888060F010400D4" }),
-    off: JSON.stringify({ "type": 1, "sid": "1616871589000", "data": "8888060F010400D4" }),
-}
+  on: JSON.stringify({
+    type: 1,
+    sid: '1616871584000',
+    data: '8888060F010400D4',
+  }),
+  off: JSON.stringify({
+    type: 1,
+    sid: '1616871589000',
+    data: '8888060F010400D4',
+  }),
+};
 
 const Nozzles = {
-    on: JSON.stringify({ "type": 1, "sid": "1616871592000", "data": "8888060F011000C8" }),
-    off: JSON.stringify({ "type": 1, "sid": "1616871596000", "data": "8888060F011000C8" }),
-}
+  on: JSON.stringify({
+    type: 1,
+    sid: '1616871592000',
+    data: '8888060F011000C8',
+  }),
+  off: JSON.stringify({
+    type: 1,
+    sid: '1616871596000',
+    data: '8888060F011000C8',
+  }),
+};
 
 async function main() {
-    const plat = new Platform("BOT-9011AC", "martas", "Spáčko");
-    const nodeLight = plat.addNode("spa", "Vířivka", ComponentType.sensor);
-    nodeLight.addProperty({
-        propertyId: "temperature",
-        dataType: PropertyDataType.float,
-        name: "Teplota",
-        propertyClass: PropertyClass.Temperature,
-        unitOfMeasurement: "°C",
-        settable: true,
-    })
+  const plat = new Platform('BOT-9011AC', 'martas', 'Spáčko');
+  const nodeLight = plat.addNode('spa', 'Vířivka', ComponentType.generic);
+  // nodeLight.addProperty({
+  //     propertyId: "temperature",
+  //     dataType: PropertyDataType.float,
+  //     name: "Teplota",
+  //     propertyClass: PropertyClass.Temperature,
+  //     unitOfMeasurement: "°C",
+  //     settable: true,
+  // })
 
-    nodeLight.addProperty({
-        propertyId: "power",
-        dataType: PropertyDataType.boolean,
-        name: "Zapnout",
-        settable: true,
-    })
+  // nodeLight.addProperty({
+  //     propertyId: "power",
+  //     dataType: PropertyDataType.boolean,
+  //     name: "Zapnout",
+  //     settable: true,
+  // })
 
-    nodeLight.addProperty({
-        propertyId: "bubbles",
-        dataType: PropertyDataType.boolean,
-        name: "Bubliny",
-        settable: true,
-        callback: function (prop) {
-            if (prop.value === "true")
-                sendData(Bubbles.on)
-            else sendData(Bubbles.off)
-        }
-    })
+  nodeLight.addProperty({
+    propertyId: 'bubbles',
+    dataType: PropertyDataType.boolean,
+    name: 'Bubliny',
+    settable: true,
+    callback: function (prop) {
+      if (prop.value === 'true') sendData(Bubbles.on);
+      else sendData(Bubbles.off);
+    },
+  });
 
-    nodeLight.addProperty({
-        propertyId: "nozzles",
-        dataType: PropertyDataType.boolean,
-        name: "Trysky",
-        settable: true,
-        callback: function (prop) {
-            if (prop.value === "true")
-                sendData(Nozzles.on)
-            else sendData(Nozzles.off)
-        }
-    })
+  nodeLight.addProperty({
+    propertyId: 'nozzles',
+    dataType: PropertyDataType.boolean,
+    name: 'Trysky',
+    settable: true,
+    callback: function (prop) {
+      if (prop.value === 'true') sendData(Nozzles.on);
+      else sendData(Nozzles.off);
+    },
+  });
 
-
-    // plat.publishData("volt", "11");
-    plat.on("connect", (client) => {
-        console.log("sending data");
-        // plat.publishSensorData("volt", "11");
-        // plat.publishSensorData("temp", "21.1");
-        // plat.publishSensorData("hum", "74");
-        // plat.publishSensorData("power", "0");
-        setInterval(function () {
-            plat.publishData("spa", "temperature", "37.8")
-        }, 10000)
-
-        plat.publishData("spa", "temperature", "38")
-    });
-    plat.init();
+  // plat.publishData("volt", "11");
+  plat.on('connect', (client) => {});
+  plat.init();
 }
 
 main();
 
 client.on('data', function (data) {
-    console.log('Received: ' + data);
-    client.destroy(); // kill client after server's response
+  console.log('Received: ' + data);
+  client.destroy(); // kill client after server's response
 });
 // client.connect(config.spaPort, config.spaIp)
 
 client.on('close', function () {
-    console.log('Connection closed');
+  console.log('Connection closed');
 });
 
 function sendData(message: string) {
-    client.connect(config.spaPort, config.spaIp, function () {
-        console.log("sending", message)
-        client.write(message);
-    })
-    // client.connect(config.spaPort, config.spaIp, function () {
-    //     console.log('Connected');
-    //     client.write(message);
-    //     console.log("message", message)
-    //     client.destroy();
-    // });
-
-
+  client.connect(config.spaPort, config.spaIp, function () {
+    console.log('sending', message);
+    client.write(message);
+  });
 }
