3efc5c2a7face548367c8bac09ba4467b1e1d514
Martin Skalický <skalima7@fit.cvut.cz>
2021-08-22 15:51:23 +0200

    Fix config

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,13 @@
 import { join } from 'path';
 
 const result = require('dotenv').config({
-  path: join(__dirname, '../.env'),
+    path: join(__dirname, '../.env'),
 });
 if (result.error) console.error(result.error);
-console.log('config', result.parsed);
 
 export default {
-  MQTT_SERVER_URL: result.parsed.MQTT_SERVER_URL,
-  MQTT_SERVER_PORT: Number(result.parsed.MQTT_SERVER_PORT),
-  STORAGE_PATH: result.parsed.STORAGE_PATH || 'local-storage',
-  HTTP_SERVER_URL: result.parsed.HTTP_SERVER_URL,
+    MQTT_SERVER_URL: process.env.MQTT_SERVER_URL,
+    MQTT_SERVER_PORT: Number(process.env.MQTT_SERVER_PORT),
+    STORAGE_PATH: process.env.STORAGE_PATH || 'local-storage',
+    HTTP_SERVER_URL: process.env.HTTP_SERVER_URL,
 };
