d08515a206620009db54ee86357e9cec1df8acdc
Martin Skalický <m.skalicky@nextap.eu>
2024-04-19 17:58:05 +0200

    Bravia handle errors

--- src/devices/bravia.ts
+++ src/devices/bravia.ts
@@ -3,6 +3,7 @@ import { Platform, DeviceStatus, ComponentType, PropertyDataType } from "https:/
 import { FactoryFn } from '../types.ts';
 import SchemaValidator, { Type, string, number, array } from 'https://denoporter.sirjosh.workers.dev/v1/deno.land/x/computed_types/src/index.ts';
 import { BraviaClient } from "./bravia/BraviaClient.ts"
+import { CallbackFn } from "https://raw.githubusercontent.com/founek2/IOT-Platform-deno/master/src/property.ts";
 
 const ActionSchema = SchemaValidator({
     name: string,
@@ -28,17 +29,33 @@ export const factory: FactoryFn<BraviaConfig> = function (config, device, logger
 
     const plat = new Platform(device.id, config.userName, device.name, config.mqtt.uri, config.mqtt.port);
 
+    function handleAction(cb: CallbackFn) {
+        const fn: CallbackFn = async (newValue): Promise<boolean | void> => {
+            try {
+                return await cb(newValue)
+            } catch (err) {
+                if (err.message?.includes('status code: 403'))
+                    logger.error('action failed, invalid pre-shared key.')
+                else logger.error('action failed. Error:', err)
+                plat.publishStatus(DeviceStatus.alert)
+                return false;
+            }
+        }
+
+        return fn;
+    }
+
     const nodeLight = plat.addNode('television', 'Televize', ComponentType.switch);
     const powerProperty = nodeLight.addProperty({
         propertyId: 'power',
         dataType: PropertyDataType.boolean,
         name: 'TV',
         settable: true,
-        callback: async (newValue) => {
+        callback: handleAction(async (newValue) => {
             if (newValue === 'true') await bravia.setPowerStatus(true);
             else await bravia.setPowerStatus(false);
             return true;
-        },
+        }),
     });
 
     const volumeProperty = nodeLight.addProperty({
@@ -47,10 +64,10 @@ export const factory: FactoryFn<BraviaConfig> = function (config, device, logger
         format: '0:80',
         name: 'TV',
         settable: true,
-        callback: async (newValue) => {
+        callback: handleAction(async (newValue) => {
             await bravia.setVolume(newValue);
             return true
-        },
+        })
     });
 
     const muteProperty = nodeLight.addProperty({
@@ -58,13 +75,13 @@ export const factory: FactoryFn<BraviaConfig> = function (config, device, logger
         dataType: PropertyDataType.boolean,
         name: 'Ztlumit',
         settable: true,
-        callback: async (newValue) => {
+        callback: handleAction(async (newValue) => {
             if (newValue == "true")
                 await bravia.muteSpeaker()
             else
                 await bravia.unmuteSpeaker()
             return true
-        },
+        }),
     });
 
     if (device.braviaActions) {
@@ -74,7 +91,7 @@ export const factory: FactoryFn<BraviaConfig> = function (config, device, logger
             format: [...device.braviaActions.map(v => v.name), "stop"].join(","),
             name: 'Zobrazit',
             settable: true,
-            callback: async (newValue) => {
+            callback: handleAction(async (newValue) => {
                 const action = device.braviaActions?.find(v => v.name === newValue)
                 if (action) {
                     await exec(`catt --device ${device.braviaIp} cast_site "${action.url}"`)
@@ -87,7 +104,7 @@ export const factory: FactoryFn<BraviaConfig> = function (config, device, logger
                 }
 
                 return false;
-            },
+            }),
         });
     }
 
