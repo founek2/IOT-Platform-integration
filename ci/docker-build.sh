c532cffb0154b537ef2450bcb370443d3f277bef
Martin Skalický <m.skalicky@nextap.eu>
2024-08-27 20:07:59 +0200

    Multiplatform build

--- ci/docker-build.sh
+++ ci/docker-build.sh
@@ -4,7 +4,9 @@ set -e
 
 name="docker-registry-write.iotdomu.cz/iot-platform/integration"
 hash=$(git rev-parse --short HEAD)
-docker build -t $name:$hash -t $name:latest --progress plain .
+
+docker buildx create --use 
+docker buildx build --platform linux/amd64,linux/arm64  -t $name:$hash -t $name:latest --progress plain .
 
 docker push $name:$hash
 docker push $name:latest
