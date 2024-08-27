3f7e5dbdbd6297e4d2a8486dfbe6af24fd3e16c3
Martin Skalický <m.skalicky@nextap.eu>
2024-08-27 22:34:55 +0200

    Fix multiplatform push

--- ci/docker-build.sh
+++ ci/docker-build.sh
@@ -5,8 +5,5 @@ set -e
 name="docker-registry-write.iotdomu.cz/iot-platform/integration"
 hash=$(git rev-parse --short HEAD)
 
-docker buildx create --use 
-docker buildx build --platform linux/amd64,linux/arm64  -t $name:$hash -t $name:latest --progress plain .
-
-docker push $name:$hash
-docker push $name:latest
+docker buildx create
+docker buildx build --platform linux/amd64,linux/arm64  -t $name:$hash -t $name:latest --push --progress plain .
