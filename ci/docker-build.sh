f224574e6193f49daa6f1230b85b8df418231229
Martin Skalický <m.skalicky@nextap.eu>
2024-09-29 12:07:18 +0200

    Move cache to different image

--- ci/docker-build.sh
+++ ci/docker-build.sh
@@ -6,4 +6,4 @@ name="docker-registry-write.iotdomu.cz/iot-platform/integration"
 hash=$(git rev-parse --short HEAD)
 
 docker buildx create --use
-docker buildx build --platform linux/amd64,linux/arm64  -t $name:$hash -t $name:latest --push --cache-from="$name" --cache-to="$name" --progress plain .
+docker buildx build --platform linux/amd64,linux/arm64  -t $name:$hash -t $name:latest --push --cache-from="$name:cache" --cache-to="$name:cache" --progress plain .
