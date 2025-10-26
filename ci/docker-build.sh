b4087b08e735ef218ad6ebf4b155ad2b0d394ce5
Martin Skalický <skalicky.martin@iotdomu.cz>
2025-10-26 18:38:45 +0100

    feat: create buildx only if not available

--- ci/docker-build.sh
+++ ci/docker-build.sh
@@ -5,5 +5,5 @@ set -e
 name="docker-registry-write.iotdomu.cz/iot-platform/integration"
 hash=$(git rev-parse --short HEAD)
 
-docker buildx create --use
+docker buildx inspect default > /dev/null || docker buildx create --use    
 docker buildx build --platform linux/amd64,linux/arm64  -t $name:$hash -t $name:latest --push --cache-from="$name:cache" --cache-to="$name:cache" --progress plain .
